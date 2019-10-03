dd0d9bba280a8205252c53386579acb2c7fe59eb
Juan Pablo Chipres Arteaga <jchipres@ucol.mx>
2019-10-03 18:33:33 -0500

    chipres

---
+++ menu/menu/menu.cpp
@@ -0,0 +1,26 @@
+// menu.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+void pausa();
+
+int main()
+{
+	bool bandera = false;
+	char tecla;
+	do 
+	{
+		system("cls");
+		cin.clear();
+		cout << 
+	}
+
+
+
+}
+
